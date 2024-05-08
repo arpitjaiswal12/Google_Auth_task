77154bc579833f0017d372ffd9fac7abe0635f0f
Arpit Jaiswal <97618151+arpitjaiswal12@users.noreply.github.com>
2024-05-08 15:53:41 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -43,7 +43,7 @@ export default function Login() {
               type="button"
               className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-blue-800 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
               onClick={() => { 
-                redirect("https://google-auth-task.vercel.app/");
+               
                 loginWithRedirect();
                 handleButtonClick(); }}
             >
